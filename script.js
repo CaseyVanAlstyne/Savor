f3a2a724180e283fab343770a798891e65910df0
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-04-19 23:47:50 -0400

    added in call function for itunes API that pulls name and genre and image and song name and song url.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var searchButton = $("#searchLyricsButton");
 var outputLyrics = $("#output");
+var resultsArray = [];
 
 // onclick function for main search button
 $("#searchLyricsButton").on("click", function () {
@@ -7,6 +8,7 @@ $("#searchLyricsButton").on("click", function () {
     var songTitle = $("#songTitle").val();
     event.preventDefault();
     findLyrics(artistName, songTitle);
+    callItunesAPI(artistName);
 })
 
 // function to pull information from lyrics.ovh api and display on screen
@@ -20,10 +22,31 @@ function findLyrics(artistName, songTitle, outputLyrics) {
             // console.log("current stuff", response.lyrics);
             var pullLyrics = response.lyrics;
             $("#output").prepend(pullLyrics);
+            // include an empty method to remove previous searches. 
         })
 }
 
-// $.get("https://api.lyrics.ovh/v1/" + document.getElementById("artistName").value + "/" + document.getElementById("songTitle").value,
-            //     function (data) {
-            //         document.getElementById("output").innerHTML = data.lyrics.replace(new RegExp("\n", "g"), "<br>")
-            //     })
+// function clearSavorites() {
+// }
+
+function callItunesAPI() {
+    var artist
+    for (var i = 0; i < resultsArray.length; i++) {
+        var artistNameFromArray = resultsArray[i]
+        var queryURL = "https://cors-anywhere.herokuapp.com/" + "https://itunes.apple.com/search?term=" + artistNameFromArray + "&limit=1";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                var result = JSON.parse(response).results;
+                artist = {
+                    name: result[0].artistName,
+                    genre: result[0].primaryGenreName,
+                    imageURL: result[0].artworkUrl100,
+                    songName: result[0].trackName,
+                    songURL: result[0].previewUrl
+                };
+            });
+    };
+}
