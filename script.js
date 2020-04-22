41813791a30c0db506a8e3b946be1062b2221613
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-04-21 20:08:43 -0400

    fixed the itunes api. Also changed the songtitle and artistname variables to classes.

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
-var searchButton = $("#searchLyricsButton");
-var outputLyrics = $("#output");
-var resultsArray = [];
+$(document).ready(function () {
 
-// onclick function for main search button
-$("#searchLyricsButton").on("click", function () {
-    var artistName = $("#artistName").val();
-    var songTitle = $("#songTitle").val();
-    event.preventDefault();
-    findLyrics(artistName, songTitle);
-    callItunesAPI(artistName);
-})
+    var searchButton = $("#searchLyricsButton");
+    var outputLyrics = $("#outputSearch");
 
-// function to pull information from lyrics.ovh api and display on screen
-function findLyrics(artistName, songTitle, outputLyrics) {
-    var queryURL = "https://api.lyrics.ovh/v1/" + artistName + "/" + songTitle;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
+    // onclick function for main search button
+    $("#searchLyricsButton").on("click", function () {
+        var artistName = $("#artistName").val();
+        var songTitle = $("#songTitle").val();
+        event.preventDefault();
+        findLyrics(artistName, songTitle);
+        callItunesAPI(artistName);
     })
-        .then(function (response) {
-            // console.log("current stuff", response.lyrics);
-            var pullLyrics = response.lyrics;
-            $("#output").prepend(pullLyrics);
-            // include an empty method to remove previous searches. 
-        })
-}
 
-// function clearSavorites() {
-// }
-
-function callItunesAPI() {
-    var artist
-    for (var i = 0; i < resultsArray.length; i++) {
-        var artistNameFromArray = resultsArray[i]
-        var queryURL = "https://cors-anywhere.herokuapp.com/" + "https://itunes.apple.com/search?term=" + artistNameFromArray + "&limit=1";
+    // function to pull information from lyrics.ovh api and display on screen
+    function findLyrics(artistName, songTitle) {
+        var queryURL = "https://api.lyrics.ovh/v1/" + artistName + "/" + songTitle;
         $.ajax({
             url: queryURL,
             method: "GET"
         })
             .then(function (response) {
+                // console.log("current stuff", response.lyrics);
+                var pullLyrics = response.lyrics;
+                $("#outputSearch").prepend(pullLyrics);
+                // include an empty method to remove previous searches. 
+            })
+    }
+
+    function callItunesAPI(artistName) {
+        console.log("hey i ran out of ideas")
+        var queryURL = "https://cors-anywhere.herokuapp.com/" + "https://itunes.apple.com/search?term=" + artistName + "&limit=1";
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        })
+            .then(function (response) {
+                console.log("hey no")
                 var result = JSON.parse(response).results;
                 artist = {
                     name: result[0].artistName,
-                    genre: result[0].primaryGenreName,
+                    // genre: result[0].primaryGenreName,
                     imageURL: result[0].artworkUrl100,
-                    songName: result[0].trackName,
-                    songURL: result[0].previewUrl
+                    // songName: result[0].trackName,
+                    // songURL: result[0].previewUrl
                 };
+                console.log("current log", result)
+                // $("#imageOutput").prepend(result);
             });
-    };
-}
+    }
+})
+
